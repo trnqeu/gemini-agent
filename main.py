441e8704cbc3bca4fb185ca9a9a38c25bfc65270
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-11-09 06:17:38 +0100

    handles prompts with sys.argv, added --verbose flag

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
-
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+    
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    user_prompt = " ".join(args)
 
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
 
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents = user_prompt
+        contents = messages
     )
 
-        
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
+
+        
+
 
 if __name__=="__main__":
     main()
